eeb933e9bb41fb6c4b55eed4ccfd81ba4af12221
diestrah <n.diestrah@pucp.edu.pe>
2026-09-24 16:09:13 -0500

    Update InvertirDigitosRecursivo.cpp

--- 02-recursion/01-nivel-basico/05-invertir-digitos-recursivo/InvertirDigitosRecursivo.cpp
+++ 02-recursion/01-nivel-basico/05-invertir-digitos-recursivo/InvertirDigitosRecursivo.cpp
@@ -12,8 +12,16 @@
 #include <iostream>
 using namespace std;
 
-int invertirNumero(int num);
-int invertirRecursivo(int num, int suma);
+int invertirNumero(int num){
+	return invertirRecursivo(num, 0);
+}
+
+int invertirRecursivo(int num, int suma){
+	if (num == 0) return suma;
+	int digito = num % 10;
+	cout << digito << " ";
+	return invertirRecursivo(num / 10, suma * 10 + digito);
+}
 
 int main(){
 	int num;
@@ -27,15 +35,3 @@ int main(){
 	
 	return 0;
 }
-
-int invertirNumero(int num){
-	return invertirRecursivo(num, 0);
-}
-
-int invertirRecursivo(int num, int suma){
-	if (num == 0) return suma;
-	int digito = num % 10;
-	cout << digito << " ";
-	return invertirRecursivo(num / 10, suma * 10 + digito);
-}
-
