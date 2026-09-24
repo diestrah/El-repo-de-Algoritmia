f594fb38ab9dd2c01adf7d85fc184c09002af497
diestrah <n.diestrah@pucp.edu.pe>
2026-09-24 16:11:39 -0500

    Update BuscarMaxArrayRecursivo.cpp

--- 02-recursion/02-nivel-intermedio/04-buscar-maximo-en-array/BuscarMaxArrayRecursivo.cpp
+++ 02-recursion/02-nivel-intermedio/04-buscar-maximo-en-array/BuscarMaxArrayRecursivo.cpp
@@ -15,7 +15,11 @@
 using namespace std;
 #define N 10
 
-void buscarMaxRecursivo(int* arrNum, int n, int& max);
+void buscarMaxRecursivo(int* arrNum, int n, int& max){
+	if (n < 0) return;
+	if (max < arrNum[n]) max = arrNum[n];
+	buscarMaxRecursivo(arrNum, n-1, max);	
+}
 
 int main(){
 	int arrNum[N] = {15, 23 ,45 ,7 ,5, 16 ,65, 84, 82, 15};
@@ -24,9 +28,3 @@ int main(){
 	cout << "El maximo es: " << max;
 	return 0;
 } 
-
-void buscarMaxRecursivo(int* arrNum, int n, int& max){
-	if (n < 0) return;
-	if (max < arrNum[n]) max = arrNum[n];
-	buscarMaxRecursivo(arrNum, n-1, max);	
-}
