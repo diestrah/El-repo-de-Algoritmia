b7e3f073ae7b6dde7a0fce8841854cfd9b504290
diestrah <n.diestrah@pucp.edu.pe>
2026-09-24 16:12:16 -0500

    Update RetornarMaxArrayRecursivo.cpp

--- 02-recursion/02-nivel-intermedio/05-retornar-maximo-de-un-array/RetornarMaxArrayRecursivo.cpp
+++ 02-recursion/02-nivel-intermedio/05-retornar-maximo-de-un-array/RetornarMaxArrayRecursivo.cpp
@@ -14,16 +14,6 @@
 using namespace std;
 #define N 10
 
-int buscarMaxRecursivo(int* arr, int n);
-int max(int a, int b);
-
-int main(){
-	int arrNum[N] = {15, 23 ,45 ,7 ,5, 16 ,65, 84, 82, 15};
-	int max = buscarMaxRecursivo(arrNum, N-1);
-	cout << "El maximo es: " << max;
-	return 0;
-} 
-
 int buscarMaxRecursivo(int *arr, int n){
 	if (n == 0) return arr[n];
 	return max(arr[n], buscarMaxRecursivo(arr, n-1));
@@ -32,6 +22,13 @@ int buscarMaxRecursivo(int *arr, int n){
 int max(int a, int b){
 	return a > b ? a : b;
 }
+
+int main(){
+	int arrNum[N] = {15, 23 ,45 ,7 ,5, 16 ,65, 84, 82, 15};
+	int max = buscarMaxRecursivo(arrNum, N-1);
+	cout << "El maximo es: " << max;
+	return 0;
+} 
 
 /*
 	Versión alternativa:
