bd16308751ee99494ed4d6de6b77d701121b11e3
Naim <n.diestrah@pucp.edu.pe>
2026-09-26 12:00:34 -0500

    Rename de las implementaciones

---
+++ 03-listas/BibliotecaListas/ElementoLista.h
@@ -0,0 +1,9 @@
+#ifndef TAD_ELEMENTOLISTA_H
+#define TAD_ELEMENTOLISTA_H
+
+struct ElementoLista{
+    int num;
+    char nom[50];
+};
+
+#endif //TAD_ELEMENTOLISTA_H
---
+++ 03-listas/BibliotecaListas/Lista.h
@@ -0,0 +1,10 @@
+#ifndef TAD_LISTA_H
+#define TAD_LISTA_H
+
+#include "NodoLista.h"
+struct Lista{
+    NodoLista* inicio;
+    int longitud;
+};
+
+#endif //TAD_LISTA_H
