42a2e2d99392964f0027150af16a023eacc201b4
diestrah <n.diestrah@pucp.edu.pe>
2026-09-25 21:10:54 -0500

    Create funciones.h

---
+++ 03-listas/03-nivel-avanzado/02-imprimir-lista-inverso-recursiva/funciones.h
@@ -0,0 +1,9 @@
+#ifndef INC_02_IMPRIMIR_LISTA_INVERSO_RECURSIVA_FUNCIONES_H
+#define INC_02_IMPRIMIR_LISTA_INVERSO_RECURSIVA_FUNCIONES_H
+
+#include "BibliotecaListas/Lista.h"
+
+void imprimirListaInversaRecursiva(const Lista& lista);
+void imprimirNodoListaInversaRecursiva(const NodoLista* nodo);
+
+#endif //INC_02_IMPRIMIR_LISTA_INVERSO_RECURSIVA_FUNCIONES_H
